735acc0e73e19879b07b9a9a647f3f4aec054ab0
Linus Nandin <79591963+LinusNandin@users.noreply.github.com>
2022-07-05 16:01:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello boi wasssup')
+    res.end('Hello boi wasssup, noew updating when file changes')
 })
 
 const PORT = process.env.PORT || 8080
