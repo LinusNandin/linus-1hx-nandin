f3864aa3a9780232e78e5fa05ed3428c79608402
Linus Nandin <79591963+LinusNandin@users.noreply.github.com>
2022-07-05 15:01:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello boi')
+    res.end('Hello boi wasssup')
 })
 
 const PORT = process.env.PORT || 8080
